d8c7d636ca4f4e712512b8f313c6818cced74078
Chris Rodriguez <christian.rodriguez6497@outlook.com>
2023-10-16 16:42:53 -0400

    Added infinite scrolling

---
+++ WeddingImageBoard/InfiniteScrolling/InfiniteScrollingItemsProviderRequest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace WeddingImageBoard.InfiniteScrolling
+{
+    public sealed class InfiniteScrollingItemsProviderRequest
+    {
+        public InfiniteScrollingItemsProviderRequest(int startIndex, CancellationToken cancellationToken)
+        {
+            StartIndex = startIndex;
+            CancellationToken = cancellationToken;
+        }
+
+        public int StartIndex { get; }
+        public CancellationToken CancellationToken { get; }
+    }
+
+    public delegate Task<IEnumerable<T>> ItemsProviderRequestDelegate<T>(InfiniteScrollingItemsProviderRequest request);
+
+}
