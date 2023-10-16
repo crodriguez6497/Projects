c8b9a24dba33c4fcf79e8f24e68d494feda518ac
Chris Rodriguez <christian.rodriguez6497@outlook.com>
2023-10-16 10:46:22 -0400

    Updated for SAS URI

--- WeddingImageBoard/Program.cs
+++ WeddingImageBoard/Program.cs
@@ -4,28 +4,10 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using WeddingImageBoard;
-using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-// key vault settings
-//var keyVaultUri = new Uri("https://weddingwebsitekeyvault.vault.azure.net/");
-//var secretClient = new SecretClient(keyVaultUri, new DefaultAzureCredential());
-
-//try
-//{
-//    var secretResponse = await secretClient.GetSecretAsync("weddingwebsitekey");
-//    var storageKey = secretResponse.Value.Value;
-//    builder.Configuration["weddingwebsitekey"] = storageKey;
-//}
-
-//catch(Exception ex)
-//{
-//    Console.WriteLine(ex.Message);
-//}
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
