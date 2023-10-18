e7a967286a7eb2e05bd37c146846c549d467a435
Chris Rodriguez <christian.rodriguez6497@outlook.com>
2023-10-18 17:09:16 -0400

    Daily updates

--- WeddingWebsiteFunction/Function1.cs
+++ WeddingWebsiteFunction/Function1.cs
@@ -9,6 +9,11 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Azure.Storage.Blobs;
 using Azure.Storage.Sas;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using System.Collections.Generic;
+using Azure.Storage.Blobs.Models;
+using Azure.Data.Tables;
+using System.Linq;
 
 namespace TestingSAS
 {
@@ -21,9 +26,9 @@ namespace TestingSAS
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string connectionString = System.Environment.GetEnvironmentVariable("PhotoStore_ConnString");
+            string connectionString = System.Environment.GetEnvironmentVariable("StorageConnectionString");
 
-            string containerName = "weddingphotoscontainertest";
+            string containerName = "weddingphotoscontainertest1";
 
             string blobName = System.Guid.NewGuid().ToString(); // Generate a new unique file name
 
@@ -52,9 +57,9 @@ namespace TestingSAS
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string connectionString = System.Environment.GetEnvironmentVariable("PhotoStore_ConnString");
+            string connectionString = System.Environment.GetEnvironmentVariable("StorageConnectionString");
 
-            string containerName = "weddingphotoscontainertest";
+            string containerName = "weddingphotoscontainertest1";
 
             BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
             BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
@@ -73,5 +78,97 @@ namespace TestingSAS
             return new OkObjectResult(sasUri);
         }
 
+
+        //Thumnail and table query
+
+        [FunctionName("TableAndThumnail")]
+        [return: Microsoft.Azure.WebJobs.Table("weddingphotostabletest1")]
+        public static async Task<MyTableEntity> Run([BlobTrigger("weddingphotoscontainertest/{name}", Connection = "StorageConnectionString")] Stream myBlob, string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            string connectionString = System.Environment.GetEnvironmentVariable("StorageConnectionString");
+            string originalBlobUrl = "https://weddingphotoscr.blob.core.windows.net/weddingphotoscontainertest/" + name;
+
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("weddingphotoscontainertest1");
+            BlobClient originalBlobClient = containerClient.GetBlobClient(name);
+            BlobProperties properties = originalBlobClient.GetProperties();
+            string userName = properties.Metadata.ContainsKey("Name") ? properties.Metadata["Name"] : "";
+
+            // generate thumbnail
+            string AICredential = System.Environment.GetEnvironmentVariable("AI_ConnString");
+            ApiKeyServiceClientCredentials credentials = new ApiKeyServiceClientCredentials(AICredential);
+            var visionClient = new ComputerVisionClient(credentials) { Endpoint = "https://weddingwebsiteaiservice.cognitiveservices.azure.com/" };
+
+            // put our image into memory stream
+            var thumbnailStream = await visionClient.GenerateThumbnailInStreamAsync(800, 800, myBlob, true);//new MemoryStream(memoryStream.ToArray()), true);
+
+            // add to thumbnail container
+            string thumbnailBlobName = Guid.NewGuid().ToString();            
+            BlobClient blobClient = new BlobClient(connectionString, "thumbnail-imagestest1", thumbnailBlobName);
+
+            await blobClient.UploadAsync(thumbnailStream, true);
+
+            // add to table
+            // Create an instance of the table entity
+            var tableEntity = new MyTableEntity
+            {
+                PartitionKey = DateTime.Now.ToString("yyyyMMddHHmmss"),
+                RowKey = thumbnailBlobName,
+                fullSizeUrl = originalBlobUrl,
+                thumbnailUrl = blobClient.Uri.ToString(),
+                name = userName
+            };
+
+            return tableEntity;
+        }
+
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+           [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+           ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            // optional variables
+            string order = req.Query.ContainsKey("order") ? req.Query["order"] : "asc";
+            int page;
+            int size;
+
+            // check for page and size, if not passed gets all
+            if (!int.TryParse(req.Query["page"], out page)) { page = 1; };
+            if (!int.TryParse(req.Query["size"], out size)) { size = int.MaxValue; }
+
+            // connect to table
+            var client = new TableServiceClient("StorageConnectionString");
+            var tableClient = client.GetTableClient("weddingphotostabletest1");
+
+            // queries table
+            var entities = tableClient.Query<Azure.Data.Tables.TableEntity>();
+
+            // gets the page were looking for
+            int recordsToSkip = (page - 1) * size;
+            var correctPage = entities.Skip(recordsToSkip).Take(size).OrderBy((x) => order.ToLower() == "dsc" ? -Int64.Parse((string)x["PartitionKey"]) : Int64.Parse((string)x["PartitionKey"])).ToList();
+
+            var jsonList = new List<string>();
+
+            // this foreach is the overinformed object, convert to the object we want
+            foreach (var record in correctPage)
+            {
+
+                jsonList.Add(JsonConvert.SerializeObject(new MyTableEntity()
+                {
+                    PartitionKey = (string)record["PartitionKey"],
+                    RowKey = (string)record["RowKey"],
+                    fullSizeUrl = (string)record["fullSizeUrl"],
+                    thumbnailUrl = (string)record["thumbnailUrl"],
+                    name = (string)record["name"]
+                }));
+
+            }
+
+            return jsonList.Count < 1 ? new NoContentResult() : new OkObjectResult(jsonList);
+        }
     }
 }
+
---
+++ WeddingWebsiteFunction/MyTableEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace TestingSAS
+{
+    public class MyTableEntity : TableEntity
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+
+        public string thumbnailName { get; set; }
+        public string fullSizeUrl { get; set; }
+        public string thumbnailUrl { get; set; }
+        public string name { get; set; }
+
+        public MyTableEntity() { }
+    }
+}
