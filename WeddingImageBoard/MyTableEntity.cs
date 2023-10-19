ec2de1ca24fd6993bfc1cc4f51a7be3f95e5fb25
Chris Rodriguez <christian.rodriguez6497@outlook.com>
2023-10-19 12:24:25 -0400

    Updated to querying table for image order. Credits to Xavier for the help on this one!

---
+++ WeddingImageBoard/MyTableEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace WeddingWebsiteThumbnailFunction
+{
+    public class MyTableEntity : TableEntity
+    {
+        public string? PartitionKey { get; set; }
+        public string? RowKey { get; set; }
+        public string? fullSizeUrl { get; set; }
+        public string? thumbnailUrl { get; set; }
+        public string? name { get; set; }
+        public DateTime? Timestamp { get; set; }
+        public string? ETag { get; set; }
+
+        public MyTableEntity() { }
+
+    }
+}
